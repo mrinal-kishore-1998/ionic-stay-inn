9bd9b931b6703b8d6d141e0ebef5e89b09e0c92e
mrinal-kishore-1998 <mrinal1117.kishore@gmail.com>
2023-02-17 11:07:16 +0530

    Adding Forward & Backward navigation using NavController

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private router: Router, private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  /**
+   * onBookPlace() is used to book the place
+   */
+  public onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover');    this uses a forward navigation animation
+    this.navCtrl.navigateBack('/places/tabs/discover');
   }
-
 }
